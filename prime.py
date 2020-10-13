a50255d2de45b8aa22db0d21e61cd1785f69270c
Tixend <63377023+Tixend@users.noreply.github.com>
2020-10-13 18:35:33 -0300

    asdasd

--- prime.py
+++ prime.py
@@ -2,7 +2,7 @@
 
 def checkIfPrimo(num):
 	if num == 1: return False
-	return sum([1 for i in range(2, num-1) if num%i == 0])==0
+	return sum([1 for x in range(2, num-1) if num%x == 0])==0
 
-numero = int(input("Ingrese su numero para verificar que sea primo:	"))
-print(checkIfPrimo(numero))
+number = int(input("Ingrese su número para verificar que sea primo:	"))
+print(checkIfPrimo(number))
