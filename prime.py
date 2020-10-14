2a6d111dc07fec27b98e4b994739b69449bf9240
Oky <72479421+Oky161@users.noreply.github.com>
2020-10-14 11:18:25 -0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,9 +4,6 @@ def checkIfPrimo(num):
 	if num == 1: return False
 	return sum([1 for x in range(2, num-1) if num%x == 0])==0
 
-number = int(input("Ingrese su número para verificar que sea primo:	"))
-print(checkIfPrimo(number))
-
 print("Verificador de numeros primos\n-------------------")
 nombre = input("Ingrese su nombre!! ")
 nombre = nombre[0].upper() + nombre[1:]
@@ -14,4 +11,3 @@ print("Bienvenido" + nombre + "!")
 
 numero = int(input("Ingrese su numero para verificar que sea primo:	"))
 print(checkIfPrimo(numero))
-print(checkIfPrimo(8))
